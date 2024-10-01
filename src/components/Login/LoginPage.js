0f31eac1436fe79a48feb8c3a508a915c05ecfe7
Sprintly Exchange <info@sprintly-exchange.com>
2024-10-01 20:48:35 +0200

    updated

--- src/components/Login/LoginPage.js
+++ src/components/Login/LoginPage.js
@@ -18,7 +18,7 @@ const LoginPage = ({ setUser }) => {
             const { username, password } = values;
 
             // Make API request to login user
-            const response = await axiosInstance.post(`${configManagerFE.getConfig('apiBaseUrl')}/api/iam/login`, { username, password });
+            const response = await axiosInstance.post(`${process.env.REACT_APP_API_BASE_URL}/api/iam/login`, { username, password });
 
             // Check response status and structure
             if (response.status === 200 && response.data.token) {
