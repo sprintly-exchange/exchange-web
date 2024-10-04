9541f6ec5b405644de5d670618e3cb4a09eafdf9
Sprintly Exchange <info@sprintly-exchange.com>
2024-10-04 19:35:22 +0200

    updated the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-# Create config.js file dynamically with environment variables
-cat <<EOF > /usr/src/app/build/config.js
-window.REACT_APP_API_BASE_URL = "${REACT_APP_API_BASE_URL}";
-EOF
-
 # Start the server to serve the built React app
 exec serve -s /usr/src/app/build
