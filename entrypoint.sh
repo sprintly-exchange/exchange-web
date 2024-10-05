46fbd48957307be1b0a834d69a9e3da6b75d683f
Sprintly Exchange <info@sprintly-exchange.com>
2024-10-05 07:31:22 +0200

    env file updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Create the .env file dynamically with environment variables
-cat <<EOF > /usr/src/app/.env
+cat <<EOF > /usr/src/app/.env.production
 REACT_APP_API_BASE_URL="${REACT_APP_API_BASE_URL}"
 REACT_APP_OTHER_ENV_VAR="${REACT_APP_OTHER_ENV_VAR}"  # Add other variables as needed
 EOF
 
 # (Optional) Add a message to indicate the file was created
 echo ".env file created with the following content:"
-cat /usr/src/app/.env  # Display the content of the .env file for verification
+cat /usr/src/app/.env.production # Display the content of the .env file for verification
 
 # Start the server to serve the built React app
 exec serve -s /usr/src/app/build
